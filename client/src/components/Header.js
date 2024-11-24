2ea695359428249d59ad696cb6299a443ff085f8
fmpbh72 <fmpassos@gmail.com>
2024-11-23 23:55:33 -0300

    corbotao

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,7 +10,7 @@ const Header = ({ title, onAdd, showAdd }) => {
       {/* <h3>process.env.EMAIL_ALUNO</h3> */}
       {location.pathname === "/" && (
         <Button
-          color={showAdd ? "green" : "red"}
+          color={showAdd ? "blue" : "purple"}
           text={showAdd ? "Add/Incluir" : "Fechar/Gravar"}
           onClick={onAdd}
         />
